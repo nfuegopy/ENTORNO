2eff0c51f70ce410489426d276ece84b9602c689
Antonio Cesar Barrios Leguizamon <antonio.barrios@berke.com.py>
2023-03-27 15:42:37 -0400

    Se agrego interfaz

--- lotter.py
+++ lotter.py
@@ -1,4 +1,5 @@
 import random
+import PySimpleGUI as sg
 
 # Creamos una lista con los números del 1 al 49
 numeros = [x for x in range(1, 50)]
@@ -6,17 +7,32 @@ numeros = [x for x in range(1, 50)]
 # Creamos una lista vacía para almacenar los números adivinados
 adivinados = []
 
-# Iteramos seis veces para elegir seis números
-for i in range(6):
-    # Elegimos un número aleatorio de la lista de números disponibles
-    numero_elegido = random.choice(numeros)
-    
-    # Agregamos el número adivinado a la lista de números adivinados
-    adivinados.append(numero_elegido)
-    
-    # Eliminamos el número adivinado de la lista de números disponibles
-    numeros.remove(numero_elegido)
+# Creamos una ventana con PySimpleGUI
+layout = [[sg.Text('Presiona el botón para adivinar los números de la lotería')],
+          [sg.Button('Adivinar'), sg.Exit()]]
 
-# Ordenamos la lista de números adivinados y la imprimimos en pantalla
-adivinados.sort()
-print("Los números adivinados son:", adivinados)
+window = sg.Window('Lotería', layout)
+
+# Iteramos mientras la ventana esté abierta
+while True:
+    event, values = window.read()
+    
+    # Si se presiona el botón de adivinar, elegimos seis números al azar
+    if event == 'Adivinar':
+        for i in range(6):
+            numero_elegido = random.choice(numeros)
+            adivinados.append(numero_elegido)
+            numeros.remove(numero_elegido)
+        
+        # Mostramos una ventana con los números adivinados
+        sg.popup("Los números adivinados son:", adivinados)
+        
+        # Reiniciamos las listas para volver a jugar
+        adivinados = []
+        numeros = [x for x in range(1, 50)]
+    
+    # Si se presiona el botón de salir, cerramos la ventana y salimos del programa
+    if event == 'Exit' or event == sg.WIN_CLOSED:
+        break
+
+window.close()
